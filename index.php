9a29ffe92966cfb31c26c393cd8db8d2d1a7eb45
SandroJaeger <76667677+SandroJaeger@users.noreply.github.com>
2021-01-06 15:03:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact </title>
+</head>
+<body>
+    <main>
+        <p>Send E-Mail<p>
+        <form class="contact-form" action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="mail" placeholder="Your E-Mail">
+        <input type="text" name="subject" placeholder="Subject">
+        <textarea name ="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">Send E-Mail</button>
+        </form>
+    
+    </main>
+</body>
+</html>
